51db8836dee12ac70c0f7e77d628f2122871219e
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-11-21 16:05:02 -0700

    working background status message

--- minecraft_utilities.py
+++ minecraft_utilities.py
@@ -1,10 +1,11 @@
 import sys
+import asyncio
 import mcstatus
 import discord
 from discord.ext import commands, tasks
 
 
-def build_embed(address):
+def create_embed(address):
     server = mcstatus.MinecraftServer.lookup(address)
     server_data = server.status().raw
 
@@ -23,14 +24,14 @@ def build_embed(address):
     return server_stats
 
 
-command_prefix = '!'
-default_server_address = '10.0.0.50'
-channel_id = 778428916616003647
-role_id = 778483486793269289 # optional
+command_prefix = '!' # config file
+default_server_address = '10.0.0.50' # database
+channel_id = 778428916616003647 # database
+role_id = 778483486793269289 # database, optional
 
 
 status_message = None
-server_message = '<@&{}> the server is online!'.format(role_id)
+ping_message = '<@&{}> the server is online!'.format(role_id)
 default_server = mcstatus.MinecraftServer.lookup(default_server_address)
 
 
@@ -40,43 +41,58 @@ bot = commands.Bot(command_prefix=command_prefix)
 @bot.event
 async def on_ready():
     print('bot is ready')
-    default_server_status.start()
+    await default_server_status.start()
 
 
 @tasks.loop(minutes=1)
-async def default_server_status(channel):
+async def default_server_status():
+    status_channel = bot.get_channel(channel_id)
     print('checking the server status...')
+    print(status_message)
 
     try:
         default_server.ping()
-        server_online(status_message)
+        await server_online(status_channel)
 
     except:
-        server_offline(status_message)
+        await server_offline()
 
 
-async def server_online(status_message):
+async def server_online(channel):
     print('server is online')
-    status_channel = bot.get_channel(channel_id)
+    global status_message
 
     if status_message != None:
-        await status_message.edit(content=server_message) #, embed=server_embed
+        print('server is still online')
+        await status_message.edit(content=ping_message)
+        print('edited status message')
 
     elif status_message == None:
-        status_message = await status_channel.send(server_message) #, embed=server_embed
-        print('sent message')
+        status_message = await channel.send(ping_message)
+        print('status message sent')
 
 
-async def server_offline(status_message):
-    print('server is offline')
+async def server_offline():
+    global status_message
+
     if status_message != None:
+        print('server went offline')
         await status_message.delete()
+        print('status message deleted')
         status_message = None
+
+    elif status_message == None:
+        print('server is offline')
 
 
 @bot.command()
 async def server_status(ctx, server_address):
-    await ctx.send(embed=build_embed(server_address))
+    await ctx.send(embed=create_embed(server_address))
+
+
+@bot.command
+async def ping(ctx):
+    print() # return bot latency
 
 
 @bot.command()
@@ -86,7 +102,7 @@ async def echo(ctx, arg):
 
 @bot.command()
 async def update(ctx): # make available to only a specific user
-    await ctx.send('Updating... Please wait at least a minute for the bot to go online again.')
+    await ctx.send('Updating... Please wait a minute for the bot to go online again.')
     sys.exit()
 
 
