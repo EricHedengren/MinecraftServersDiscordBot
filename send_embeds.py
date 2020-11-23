2da8c01093974f78c7bb54c2f8f4b67cbbbd598f
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-11-22 20:33:57 -0700

    send embeds variable config

--- send_embeds.py
+++ send_embeds.py
@@ -1,14 +1,19 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.mu ')
-
+color = 0x5796d9
+title = "Michael's Server"
+description = "`136.36.192.233`\n\nThis server is cross platform between Bedrock and Java, and usually hosts a vanilla world. It's great for playing casual Minecraft with friends."
+thumbnail_url = 'https://cdn.discordapp.com/emojis/759901750281895999.png?v=1'
 channel_id = 772220260589240363
 
-embed = discord.Embed(color=0x5796d9, title="Michael's Server", description="`136.36.192.233`\n\nThis server is cross platform between Bedrock and Java, and usually hosts a vanilla world. It's great for playing casual Minecraft with friends.")
 
-embed.set_thumbnail(url='https://cdn.discordapp.com/emojis/759901750281895999.png?v=1')
+embed = discord.Embed(color=color, title=title, description=description)
 
+embed.set_thumbnail(url=thumbnail_url)
+
+
+bot = commands.Bot(command_prefix='.mu ')
 
 @bot.event
 async def on_ready():
