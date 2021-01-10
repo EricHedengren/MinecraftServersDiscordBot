a8f3d8a5a4ff63dac6f005c61a976033dbe75d5d
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2021-01-10 16:00:22 -0700

    bold seconds

--- minecraft_utilities.py
+++ minecraft_utilities.py
@@ -152,7 +152,7 @@ async def server(ctx, address):
 async def info(ctx):
     version = "Version: **{}**".format(bot_version)
     latency = "Latency: **{:.2f}** ms".format(bot.latency * 1000)
-    runtime = "Runtime: {:.0f} s".format(time.time()-start_time)
+    runtime = "Runtime: **{:.0f}** s".format(time.time()-start_time)
 
     await ctx.send('\n'.join([version, latency, runtime]))
 
