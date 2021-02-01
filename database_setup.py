532a1b961ffbfb4952c6feeca39c88ca33695208
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2021-01-31 21:13:32 -0700

    added on delete cascade: easy guild data removal

--- database_setup.py
+++ database_setup.py
@@ -9,7 +9,7 @@ except:
 
 # create table
 discord_table = 'CREATE TABLE discord (guild_id int NOT NULL, channel_id int NOT NULL, role_id int, PRIMARY KEY (guild_id));'
-address_table = 'CREATE TABLE address (guild int NOT NULL, ip_address text NOT NULL, server_name text, message_id int, PRIMARY KEY (guild,ip_address), FOREIGN KEY (guild) REFERENCES discord (guild_id));'
+address_table = 'CREATE TABLE address (guild_id int NOT NULL, ip_address text NOT NULL, server_name text, message_id int, PRIMARY KEY (guild_id,ip_address), FOREIGN KEY (guild_id) REFERENCES discord (guild_id) ON DELETE CASCADE);'
 
 tables = (discord_table, address_table)
 
