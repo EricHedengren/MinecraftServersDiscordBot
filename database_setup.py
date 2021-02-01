8cb3e3376db6993e9871d44382918dd0a764f5ed
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2021-01-31 20:26:34 -0700

    not null implementation

--- database_setup.py
+++ database_setup.py
@@ -8,8 +8,8 @@ except:
     print('could not connect')
 
 # create table
-discord_table = 'CREATE TABLE discord (guild_id int PRIMARY KEY, channel_id int, role_id int);'
-address_table = 'CREATE TABLE address (guild int, ip_address text, server_name text, message_id int, PRIMARY KEY (guild,ip_address), FOREIGN KEY (guild) REFERENCES discord (guild_id));'
+discord_table = 'CREATE TABLE discord (guild_id int NOT NULL, channel_id int NOT NULL, role_id int, PRIMARY KEY (guild_id));'
+address_table = 'CREATE TABLE address (guild int NOT NULL, ip_address text NOT NULL, server_name text, message_id int, PRIMARY KEY (guild,ip_address), FOREIGN KEY (guild) REFERENCES discord (guild_id));'
 
 tables = (discord_table, address_table)
 
